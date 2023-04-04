7ed6eba1e48588249b424e8c75b678b21adb6164
mrityunjay mishra <mrityunjay.83039@gmail.com>
2023-04-04 14:53:08 +0530

    daynamic image done on single page

--- src/components/MyImage.js
+++ src/components/MyImage.js
@@ -1,8 +1,86 @@
+import { useState } from "react";
+import styled from "styled-components";
 
-const MyImage = () => {
-    return(
-        <></>
-    );
+
+const MyImage = ({imgs = [{url : " "}]}) => {
+   
+    // use state to change main image according to clicked image
+    const[mainImage, setMainImage] = useState(imgs[0]);
+
+const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: 0.4fr 1fr;
+  gap: 1rem;
+
+  .grid {
+    flex-direction: row;
+    justify-items: center;
+    align-items: center;
+    width: 100%;
+    gap: 1rem;
+    /* order: 2; */
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      background-size: cover;
+      object-fit: contain;
+      cursor: pointer;
+      box-shadow: ${({ theme }) => theme.colors.shadow};
+    }
+  }
+
+  .main-screen {
+    display: grid;
+    place-items: center;
+    order: 1;
+    img {
+      max-width: 100%;
+      height: auto;
+      box-shadow: ${({ theme }) => theme.colors.shadow};
+    }
+  }
+  .grid-four-column {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    display: flex;
+    flex-direction: column;
+    order: 1;
+
+    .grid-four-column {
+      grid-template-rows: 1fr;
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+`;
+
+return(
+    <Wrapper>
+        <div className="grid grid-four-column">
+
+            {imgs.map((curElem, index)=>{
+                return(
+                    <figure>
+                        <img
+                        src={curElem.url}
+                        alt={curElem.filename}
+                        onClick={() => setMainImage(curElem)}
+                        />
+                    </figure>
+                );
+            })}
+
+        </div>
+        <div className="main-screen">
+            <img src={mainImage.url} alt={mainImage.filename} />
+        </div>
+    </Wrapper>
+);
+
+     
 }
 
 export default MyImage;
